554d944fc5a2acbc2c39987324350ad1f6f59a88
Vasundhara vats <vatsv2491@gmail.com>
2025-09-03 23:24:00 +0530

    Day 15

--- App.jsx
+++ App.jsx
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
 
 function App() {
+  const [auth, setAuth] = useState(!!localStorage.getItem("token"));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login setAuth={setAuth} />} />
+        <Route
+          path="/dashboard"
+          element={
+            auth ? <Dashboard /> : <Navigate to="/login" />
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
